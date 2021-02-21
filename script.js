31f6c767acc042063b1a1c8b82c39aa87c38e183
cory alport <cralport@gmail.com>
2021-02-21 15:26:25 -0700

    almost fixed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "()"]
-const choices = [];
+var choices = [];
 
 var confirmLower;
 var confirmUpper;
@@ -21,52 +21,65 @@ function generatePassword() {
   var length = window.prompt("How many characters will your password be? (Choose between 8 and 128)")
 
   if (length >= 8 && length <= 128) {
-    console.log("good length")
+
   } else {
-    console.log("bad length")
+
     return ("");
 
   }
 
 
   confirmLower = window.confirm("Do you want lower case letters");
-  console.log(lowerCase)
-  if (lowerCase == true) {
-    choices = [lowerCase]
+
+  if (confirmLower == true) {
+    choices = (choices.concat(lowerCase))
   };
 
   confirmUpper = window.confirm("Do you want upper case letters");
-  console.log(upperCase)
-  if (upperCase == true) {
-    choices = [upperCase]
-   };
 
-  confrimNumbers = window.confirm("Do you want numbers");
-  console.log(numbers)
-  if (numbers == true) {
-    choices = [numbers]
-   };
-
-  confrimSymbols = window.confirm("Do you want symbols");
-  console.log(symbols)
-  if (symbols == true) { 
-    choices = [symbols]
+  if (confirmUpper == true) {
+    choices = (choices.concat(upperCase))
   };
 
-  if (confirmLower === false && confirmUpper === false && confrimNumbers === false && confrimSymbols === false) {
+  confirmNumbers = window.confirm("Do you want numbers");
+
+  if (confirmNumbers == true) {
+
+    choices = (choices.concat(numbers))
+  };
+
+  confirmSymbols = window.confirm("Do you want symbols");
+
+  if (confirmSymbols == true) {
+    choices = (choices.concat(symbols))
+
+  };
+
+  if (confirmLower === false && confirmUpper === false && confirmNumbers === false && confirmSymbols === false) {
     alert("At least one character needs to be selected");
+    return ("");
+
   }
+
+
   console.log("checking", (lowerCase === false && upperCase === false && numbers === false && symbols === false))
   console.log(choices)
-  return ("");
+
+  var password = "";
+  console.log("password")
+  console.log(choices.length)
+  for (var i = 0; i < length; i++) {
+    password += (choices[char])
+    var char = Math.floor(Math.random() * choices.length);
+    console.log("what is char", choices[char])
+  }
+  return (password);
+
+
 
 }
 
-var password = "";
 
-for (var i = 0; i < choices.length; i++) {
-  var password = Math.floor(Math.random() * choices.length);
-}
 
 
 console.log("numbers")
